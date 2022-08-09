777146f632d2ca9cd8cbd19732a9ac4e7c93cada
Azeezjim <azeexjim@gmail.com>
2022-08-09 03:55:26 -1100

    formik_first commit of the day

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FormikContainer from './component/FormikContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <FormikContainer />
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ src/component/FormikContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Formik, Form } from 'formiok';
+import * as Yup from 'yup'
+
+function FormikContainer() {
+  const initialValues = {}
+  const validationSchema = Yup.object({})
+  const onSubmit = (values) => console.log('form data', values);
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {
+        formik => <Form>
+          <submit type="submit">Submit</submit>
+        </Form>
+      }
+    </Formik>
+}
+
+export default FormikContainer
